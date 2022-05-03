50ebab5c65e35992750f2c60da0351f30006dcb5
CreamToby <hyson09@gmail.com>
2022-05-03 15:42:09 -0400

    add topping

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Menu from "./menu/Menu";
+
+const menuItems = [
+  {
+    id: 1,
+    type: 'Cheese',
+    price: 2,
+    qty: 0
+  },
+  {
+    id: 2,
+    type: 'Pepperoni',
+    price: 1.5,
+    qty: 0
+  },
+  {
+    id: 3,
+    type: 'Sausage',
+    price: 1.5,
+    qty: 0
+  },
+  {
+    id: 4,
+    type: 'Mushroom',
+    price: 0.75,
+    qty: 0
+  },
+  {
+    id: 5,
+    type: 'Pepper',
+    price: 0.75,
+    qty: 0
+  },
+  {
+    id: 6,
+    type: 'Olive',
+    price: 1.5,
+    qty: 0
+  },
+  {
+    id: 7,
+    type: 'Pineapple',
+    price: 1.5,
+    qty: 0
+  },
+  {
+    id: 8,
+    type: 'Ham',
+    price: 1.5,
+    qty: 0
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>Hello from the App Component</h1> */}
+      {/* Passing the menuItems array (Array of objects) as the value for the startingItems prop in the Menu Component */}
+      <Menu startingItems={menuItems}/>
     </div>
   );
 }
